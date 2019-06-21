229ce6f334ffb07bcb4d643773876f93b98154ea
Fredyss <34088845+Fredyss@users.noreply.github.com>
2019-06-21 08:08:52 +0200

    correction

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -1,68 +1,87 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import './Pokedex.scss';
+import "./Pokedex.scss";
 
 export default class Pokedex extends React.Component {
-    constructor (props)  {
-      super(props);      
-      this.state = {
-        pokemons: [],
-        isToggleOn: false
-      }
-      this.handleClick = this.handleClick.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons: [],
+      indexPokemon: null
+    };
+  }
 
-    }
-  
-    handleClick() {
-      this.setState(state => ({
-        isToggleOn: !state.isToggleOn
+  handleClick(index) {
+    this.setState(state => ({
+      indexPokemon: index
+    }));
+  }
+
+  componentDidMount() {
+    this.getPokemons();
+  }
+  getPokemons = async () => {
+    for (let i = 1; i < 10; i++) {
+      const res = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon/" + i.toString()
+      );
+
+      // getting name of pokemon
+      const namePokemon = res.data.forms[0].name;
+      // console.log(namePokemon);
+
+      // getting img path pokemon
+      const imgPokemon = res.data.sprites.front_default;
+      // console.log(imgPokemon);
+
+      // pushing pokemons to state
+      this.setState(prevState => ({
+        pokemons: [...prevState.pokemons, { namePokemon, imgPokemon }]
       }));
-      console.log(this.state.isToggleOn);
+    }
+    // console.log(this.state);
+  };
+
+  render() {
+    let pokemon;
+    let pokemonList;
+    if (this.state.pokemons.length === 0) {
+      pokemonList = (<h1>Loading</h1>)
+    }
+    if (this.state.pokemons.length > 1) {
+      pokemonList = (
+        <div>
+            {this.state.pokemons.map((e, i) => {
+              return (
+                <article key={i} onClick={() => this.handleClick(i)}>
+                  <p>{e.namePokemon}</p>
+                </article>
+              );
+            })}
+        </div>
+      )
     }
 
-  
-
-    componentDidMount() {
-       this.getPokemons();
+    if (this.state.indexPokemon !== null) {
+      const k = this.state.indexPokemon;
+      // console.log(k);
+      // console.log(this.state.pokemons[k].namePokemon);
+      // show selected pokemon
+      pokemon = (
+      <div className="pokemon">
+        <h2>Your pokemon is</h2>
+        <img src={this.state.pokemons[k].imgPokemon} alt={this.state.pokemons[k].namePokemon} />
+        <p>{this.state.pokemons[k].namePokemon}</p>
+      </div>
+      )
     }
-    getPokemons = async () => {
-      for(let i = 1; i< 10; i++ ){
-        const res = await axios.get("https://pokeapi.co/api/v2/pokemon/"+i.toString());
 
-        // getting name of pokemon
-        const namePokemon = res.data.forms[0].name;
-        // console.log(namePokemon);
-
-        // getting img path pokemon
-        const imgPokemon = res.data.sprites.front_default;
-        // console.log(imgPokemon);
-     
-        // pushing pokemons to state
-        this.setState(prevState => ({
-          pokemons: [...prevState.pokemons, {namePokemon, imgPokemon}]
-        }))
-      
-      }
-      // console.log(this.state)  
-};
-
-    render() {
-       return (
-           <main>
-               {this.state.pokemons.length === 0 ? (
-                   <div>Loading...</div>
-               ) : (
-                   this.state.pokemons.map((e, i) => {
-                       return <article  key={i}
-                              onClick={this.handleClick}
-                              className= {this.state.isToggleOn ? "show" :""}>
-                              <img src={e.imgPokemon} alt={e.namePokemon}/>
-                            <p>{e.namePokemon}</p>
-                         </article>;
-                    })
-               )}
-           </main>
-       );
-     }
+    return (
+      <main>
+        {pokemonList}
+        {pokemon}
+      </main>
+    );
+  }
 }
